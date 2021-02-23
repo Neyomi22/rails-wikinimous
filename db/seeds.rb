2965d1dbf77c4b91cc3602cdd24b68e6a4e00120
Amanda Paranamana <neyomi22@hotmail.com>
2021-02-23 17:00:27 +1100

    Created the methods for Rails wikinimous

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,10 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do
+  Article.create(
+    title: Faker::Book.title,
+    content: Faker::Games::WorldOfWarcraft.quote
+  )
+end
